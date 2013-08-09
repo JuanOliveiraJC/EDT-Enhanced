61f125f8dd43b16d2a38ba31d0d0bca31b9ace90
madzomboy <fodaonacamaui@hotmail.com>
2013-08-08 21:39:55 -0300

    mudança em algumas palavras

--- TCR-Enhanced.js
+++ TCR-Enhanced.js
@@ -83,11 +83,11 @@ TCREnhancedModel = require('app/base/Class').extend({
         API.on(API.CHAT,this.proxy.onChat)
         API.on(API.CHAT_COMMAND,this.customChatCommand)
          var a = $('#chat-messages'),b = a.scrollTop() > a[0].scrollHeight - a.height() - 20;
-        a.append('<div class="chat-update"><span class="chat-text" style="color:#FF0000"><i>Bem vindo a sala oficial do blog TRETA, script criado por bruce versão ' + this.version.major + '.' + this.version.minor + '.' + this.version.patch + '</i></span></div>');
-        a.append('<div class="chat-update"><span style="color:#FFFF00">Entre para nosso grupo <a href="http://goo.gl/FjOaxp" target="_blank">grupo</a>!</span></div>');
-        a.append('<div class="chat-update"><span style="color:#FFFF00">Curta nossa <a href="http://goo.gl/jhO9Nq" target="_blank">página</a>!</span></div>');
+        a.append('<div class="chat-update"><span class="chat-text" style="color:#FF0000"><i>Bem vindo a sala oficial do blog  <a target="_blank" href="http://www.treta.com.br">TRETA</a>, script criado por bruce versão ' + this.version.major + '.' + this.version.minor + '.' + this.version.patch + '</i></span></div>');
+        a.append('<div class="chat-update"><span style="color:#FFFF00">Entre para nosso grupo <a target="_blank" href="http://goo.gl/FjOaxp" title="Rola altas tretas aqui">grupo</a>!</span></div>');
+        a.append('<div class="chat-update"><span style="color:#FFFF00">Curta nossa <a target="_blank" href="http://goo.gl/jhO9Nq" title="Ta esperando o que pra curtir ?!">página</a>!</span></div>');
         a.append('<div class="chat-update"><a target="_blank" href="http://www.treta.com.br" title="Mete o dedo na tomada!">((( TRETA )))</a></div>');
-        a.append('<div class="chat-update"><span style="color:#FFFF00">Dicas e regras da sala <a href="http://goo.gl/7De903" target="_blank">aqui</a> leia se for novato!</div>')
+        a.append('<div class="chat-update"><span style="color:#FFFF00">Dicas e regras da sala <a  target="_blank" href="http://goo.gl/7De903" title="Clica logo">aqui</a> leia se for novato!</div>')
         this.removeElements();
         if (plugCubed == undefined) $.getScript("http://plugCubed.com/compiled/plugCubed.min.js")
     },
