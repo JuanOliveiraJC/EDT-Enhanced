352e57ac0bbe11848525ef5a118d75c22fb126c6
madzomboy <fodaonacamaui@hotmail.com>
2013-08-09 21:05:38 -0300

    Update TCR-Enhanced.js

--- TCR-Enhanced.js
+++ TCR-Enhanced.js
@@ -152,7 +152,7 @@ TCREnhancedModel = require('app/base/Class').extend({
         $('#button-dj-waitlist-leave').attr('style','background-image:url(http://i.imgur.com/i4YkTFC.png); display: block;');
         $('#button-dj-play').attr('style','background-image:url(http://i.imgur.com/SqU01C6.png); display: block;');
         $('#button-dj-leave').attr('style','background-image:url(http://i.imgur.com/i4YkTFC.png); display: block;');
-        $('#dj-console').attr('style','background-image:url(http://i.imgur.com/IpyhanS.gif); display:block; position:absolute; top:15px; width:317px;');
+        $('#dj-console').attr('style','background-image:url(https://dl.dropboxusercontent.com/u/198705975/dj_booth4.gif); display:block; position:absolute; top:15px; width:317px;');
         $('#button-add-this').attr('style','background-image:url(https://dl.dropboxusercontent.com/u/198705975/add_this.png);');
         $('#meta-frame').css('background-color','transparent');
         $('#playback .frame-background').hide('.frame-background');
