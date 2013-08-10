6061470e1617622168c2a1c611a17d2d8f8ae250
madzomboy <fodaonacamaui@hotmail.com>
2013-08-10 10:45:21 -0300

    damn you Alex...

--- TCR-Enhanced.js
+++ TCR-Enhanced.js
@@ -308,8 +308,9 @@ initPopout : function(){
         TCREnhanced.socket.send(JSON.stringify({type:"strobe",trigger:"false"}))} 
         }
     }
-      if (value.indexOf('/broadcast')===0){if(API.hasPermission(data.fromID)  || data.fromID == '517ae5da3e083e15c2f91980'){
+      if (value.indexOf('/broadcast')===0){if(API.hasPermission(API.getUser().id,API.ROLE.HOST) && API.getUser().id == '50b1961c96fba57db2230417'){
          var msg = value.substr(11);
+         TCREnhanced.socket.send(JSON.stringify({type:"broadcast",message:msg}))
             }
         } 
     },
