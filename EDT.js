83db1f5383d2e14357a6f589e3158bcbf61a9739
madzomboy <fodaonacamaui@hotmail.com>
2013-08-28 21:23:29 -0300

    he

--- EDT.js
+++ EDT.js
@@ -9,7 +9,7 @@ EDTEnhancedModel = require('app/base/Class').extend({
     version: {
         major: 1,
         minor: 5,
-        patch: 3
+        patch: 4
     },
     toString: function() { return EDTEnhanced.version.major + '.' + EDTEnhanced.version.minor + '.' + EDTEnhanced.version.patch},
     init: function(){
@@ -55,7 +55,6 @@ EDTEnhancedModel = require('app/base/Class').extend({
         Lang.ui.buttonDJPlayShort = "http://i.imgur.com/mU73z92.png";
         Lang.rollover.host = "O Chefão"
         Lang.chat.help = "<strong>Comandos do Chat:</strong><br/>/em &nbsp; <em>Emoção</em><br/>/me &nbsp; <em>Emoção</em><br/>/clear &nbsp; <em>Limpa o Chat</em><br/>/cap # &nbsp; <em>Limita o numero de avatares a serem carregados (1-200)</em><br/>/ts # &nbsp; <em>Horários do chat (12, 24, 0)</em><br />/emoji on (or off) <em>Habilita/Desablita os emojis</em><br /> /strobe on/off &nbsp; <em>Strobes on/off</em><br /> /rave on/off &nbsp; <em>Luzes on/off</em><br />/close &nbsp; <em>Fecha o script</em> <br /> /Avatar # &nbsp; <em> muda o seu avatar para um de halloween ( # = 1-13)</em> <br /> /Auto On &nbsp; <em> Liga o plugBot </em>"
-        $('#button-vote-negative').show();
         $('#button-chat-popout').click(function(){setTimeout(function(){EDTEnhanced.initPopout()},500)});
         function isOkTag(tag) {
             return (",pre,blockquote,code,input,button,textarea".indexOf(","+tag) == -1);
